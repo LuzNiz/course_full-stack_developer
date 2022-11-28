6e77019292005945c63b6bdecaefec375e207f92
Luz Niz <110864533+LuzNiz@users.noreply.github.com>
2022-11-28 12:35:00 -0300

    Delete louvre_trabajo_final_cuatrimestre directory

--- louvre_trabajo_final_cuatrimestre/ts/events.js
+++
@@ -1,8 +0,0 @@
-function cambiarValorBoton(span) {
-    if (span.innerHTML === "Ver menos") {
-        span.innerText = "Ver m\u00E1s";
-    }
-    else {
-        span.innerText = "Ver menos";
-    }
-}
